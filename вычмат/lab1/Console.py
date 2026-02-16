097527e0cb900b41b5dc85d30073b28c1c0aad02
Алексей Поляков <467141@niuitmo.ru>
2026-02-16 12:52:30 +0300

    v1.0

---
+++ вычмат/lab1/Console.py
@@ -0,0 +1,125 @@
+from Matrix import Matrix
+
+def matrix_interaction(matrix):
+    print("Матрица успешно создана")
+    matrix.calculate()
+    while True:
+        action = input("""Выберите действие:
+    0.Сравнить результат с готовыми библиотеками
+    1.Вывести определитель
+    2.Вывести треугольную матрицу
+    3.Вывести вектор неизвестных
+    4.Вывести вектор невязок
+    5.Ввести новую матрицу
+    6.Завершить программу
+""")
+        try:
+            action = int(action)
+        except ValueError:
+            print("Некорректный ввод, повторите попытку")
+            continue
+        match action:
+            case 0:
+                print(matrix.compare())
+            case 1:
+                print(matrix.det)
+            case 2:
+                for i in matrix.triangle_matrix:
+                    print(i)
+            case 3:
+                if abs(matrix.det) < 1e-12:
+                    print("Матрица вырождена и не имеет единтвенного решения")
+                    continue
+                print(matrix.answer)
+            case 4:
+                if abs(matrix.det) < 1e-12:
+                    print("Матрица вырождена и не имеет единтвенного решения")
+                    continue
+                print(matrix.residual)
+            case 5:
+                break
+            case 6:
+                exit()
+
+
+def from_console():
+    while True:
+        n = input("Введите размерность матрицы: (n<=20)\n")
+        try:
+            n = int(n)
+        except ValueError:
+            print("Некорректный ввод, повторите попытку")
+            continue
+        if n > 20:
+            print("Размерность должны быть <=20, повторите попытку")
+            continue
+        break
+    matrix = Matrix(n)
+    print("""Введите СЛАУ в виде расширеной матрицы:
+    Пример правильного ввода:
+    1 2 3
+    4 5 6""")
+    for _ in range(n):
+        while True:
+            row = input().split()
+            try:
+                row = [int(i) for i in row]
+            except ValueError:
+                print("Некорректный ввод, повторите попытку")
+            if len(row) == n + 1:
+                matrix.append(row)
+                break
+            print("Некорректный ввод, повторите попытку")
+    matrix_interaction(matrix)
+
+
+def from_file():
+    path = input("""Введите путь к файлу
+Формат файла - размерность в первой строке, далее СЛАУ в виде расширеной матрицы
+Размерность должны быть <=20
+""")
+    with open(path, "r", encoding="utf-8") as f:
+        file = f.read().splitlines()
+    n = 0
+    try:
+        n = int(file[0])
+    except ValueError:
+        print("Некорретные параметры в файле")
+        return
+    if n > 20:
+        print("Размерность должны быть <=20, повторите попытку")
+        return
+    matrix = Matrix(n)
+    if len(file) != n + 1:
+        print("Некорретные параметры в файле")
+        return
+    for i in file[1:]:
+        row = []
+        try:
+            row = [int(x) for x in i.split()]
+        except ValueError:
+            print("Некорретные параметры в файле")
+            return
+        if len(row) == n+1:
+            matrix.append(row)
+            continue
+        print("Некорретные параметры в файле")
+        return
+    matrix_interaction(matrix)
+def main():
+    while True:
+        method = input("""Выберите способ введения СЛАУ:
+    1.Через консоль
+    2.Из файла
+""")
+        match method:
+            case "1":
+                from_console()
+            case "2":
+                from_file()
+            case _:
+                print("Некорректный ввод, повторите попытку")
+
+
+if __name__ == "__main__":
+    main()
---
+++ вычмат/lab1/Matrix.py
@@ -0,0 +1,86 @@
+import numpy as np
+
+class Matrix:
+    eps = 1e-12
+
+    def __init__(self, n):
+        self.n = n
+        self.matrix = []
+        self.triangle_matrix = []
+        self.det = 0
+        self.answer = [0] * n
+        self.residual = [0] * n
+
+    def append(self, row):
+        self.matrix.append(row)
+
+    def set_matrix(self, matrix):
+        self.matrix = self.copy_matrix(matrix)
+
+    def find_answer(self):
+        triangle_matrix = self.copy_matrix(self.triangle_matrix)
+        if abs(self.det) < self.eps: return
+        for i in range(self.n - 1, -1, -1):
+            self.answer[i] = triangle_matrix[i][-1]/triangle_matrix[i][i]
+            for j in range(i):
+                triangle_matrix[j][-1] -= triangle_matrix[j][i] * self.answer[i]
+                triangle_matrix[j][i] = 0
+
+    def find_residual(self):
+        if abs(self.det) < self.eps: return
+        for i in range(self.n):
+            self.residual[i] = self.matrix[i][-1]
+            for j in range(self.n):
+                self.residual[i] -= self.matrix[i][j] * self.answer[j]
+
+    def calculate(self):
+        n = self.n
+        matrix = self.copy_matrix(self.matrix)
+        count = 0
+        for i in range(n):
+            l = i
+            for m in range(i + 1, n):
+                if abs(matrix[m][i]) > abs(matrix[l][i]):
+                    l = m
+            if l != i:
+                count += 1
+                matrix[i], matrix[l] = matrix[l], matrix[i]
+            for j in range(i + 1, n):
+                if abs(matrix[i][i]) < self.eps:
+                    self.det = 0
+                    return
+                cf = -matrix[j][i] / matrix[i][i]
+                matrix[j] = [matrix[j][k] + cf * matrix[i][k] for k in range(len(matrix[i]))]
+        self.triangle_matrix = matrix
+        self.det = (-1) ** count
+        for i in range(n):
+            self.det *= self.triangle_matrix[i][i]
+        self.find_answer()
+        self.find_residual()
+
+    def copy_matrix(self, matrix):
+        return [row[:] for row in matrix]
+
+
+    def compare(self):
+        A = np.array([row[:-1] for row in self.matrix], dtype=float)
+        b = np.array([row[-1] for row in self.matrix], dtype=float)
+        answer_numpy = np.linalg.solve(A, b)
+        det_numpy = np.linalg.det(A)
+        residual_numpy = b - A @ answer_numpy
+
+        return (f"""Под пунктом 1 записывается результат, полученный с помощью Метода Гаусса
+Под пунктом 2 записывается результат, полученный использованием готовй библиотеки NumPy
+
+Определители:
+1.{self.det}
+2.{det_numpy}
+
+Векторы неизвестных:
+1.{self.answer}
+2.{answer_numpy.tolist()}
+
+Векторы невязок:
+1.{self.residual}
+2.{residual_numpy.tolist()}
+""")
